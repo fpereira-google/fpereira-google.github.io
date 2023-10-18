5cb1045ba9e64877db07959bf7ff8dac7a26b054
fpereira800 <fpereira.oliveira800@gmail.com>
2023-10-18 15:49:39 -0300

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector("#btn-menu")
+menu.addEventListener("click", () => {
+    console.log("clicou")
+})
+
+let sorte = document.querySelector("#btn-sorte")
+sorte.addEventListener("click", () => {
+    location.href = "https://class.hcode.com.br"
+})
